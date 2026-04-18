3679d209d4f4b7fe83dc851c462cc2d7268551af
Keerthi Sreenivas <69894872+Keerthi-Sreenivas@users.noreply.github.com>
2026-04-18 11:09:27 -0700

    Add descriptions to all Terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,14 @@
 variable "region" {
-  default = "us-east-1"
+  default     = "us-east-1"
+  description = "AWS region for the EKS cluster"
 }
 
 variable "gpu_min_nodes" {
-  default = 1
+  default     = 1
+  description = "Minimum number of GPU nodes"
 }
 
 variable "gpu_max_nodes" {
-  default = 5
+  default     = 5
   description = "Maximum number of GPU nodes for autoscaling"
 }
